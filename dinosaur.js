2e428f5a1be6f43232e67266152414c7345cfbb7
Matt Rose <yamiacat@gmail.com>
2017-06-13 17:50:12 +0100

    Add Dinosaur class

---
+++ dinosaur.js
@@ -0,0 +1,9 @@
+var Dinosaur = function(type, annualOffspring) {
+  this.type = type;
+  this.annualOffspring = annualOffspring || 10;
+}
+
+
+
+
+module.exports = Dinosaur;
--- specs/dinosaur_spec.js
+++ specs/dinosaur_spec.js
@@ -8,6 +8,19 @@ describe("Dinosaur", function() {
     dinosaur1 = new Dinosaur("Tyranosaur");
   });
 
+  it("should have a type", function() {
+    assert.strictEqual("Tyranosaur", dinosaur1.type);
+  });
+
+  it("should have a default annual offspring of 10", function() {
+    assert.strictEqual(10, dinosaur1.annualOffspring);
+  });
+
+
+  it("should allow annual offspring to be set", function() {
+    var dinosaur2 = new Dinosaur("Stegosaur", 20)
+    assert.strictEqual(20, dinosaur2.annualOffspring);
+  });
 
 
 });
